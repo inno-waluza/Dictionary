611fc0b80d19227ec6a7ceaf5f52ebc1320f1ca1
innowaluza <innowaluza@gmail.com>
2023-03-08 15:34:04 +0200

    added while loop and cleaned up the code

--- Dictionary/Dictionary.py
+++ Dictionary/Dictionary.py
@@ -1,34 +1,32 @@
 import requests
 import json
-print("Dictionary")
 
-input = input("enter word")
-def get_meaning(input):
-    data = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{input}")
-    response = data.text
-    return response
+while True:
+    print("Dictionary")
+    word_input = input("enter word: ")
+    def get_meaning(word):
+        data = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+        response = data.text
+        return response
+    # Load the JSON data from a file or a string
+    json_data = get_meaning(word_input) 
 
-# Load the JSON data from a file or a string
-json_data = get_meaning(input) 
+    # Load the JSON data as a Python object
+    data = json.loads(json_data)
 
-# Load the JSON data as a Python object
-data = json.loads(json_data)
+    # Extract the word 
+    word = data[0]["word"]
 
-# Extract the word 
-word = data[0]["word"]
+    definitions = []
+    for meaning in data[0]["meanings"]:
+        part_of_speech = meaning["partOfSpeech"]
+        for definition in meaning["definitions"]:
+            definition_text = definition["definition"]
+            definitions.append({"part_of_speech": part_of_speech, "definition_text": definition_text})
 
-definitions = []
-for meaning in data[0]["meanings"]:
-    part_of_speech = meaning["partOfSpeech"]
-    for definition in meaning["definitions"]:
-        definition_text = definition["definition"]
-        definitions.append({"part_of_speech": part_of_speech, "definition_text": definition_text})
-
-# Print the extracted data
-print(f"Word: {word}")
-print("Definitions:")
-for definition in definitions:
-    print(f"{definition['part_of_speech']}: {definition['definition_text']}")
-
-
-  
+    # Print the extracted data
+    print(f"Word: {word}")
+    print("Definitions:")
+    for definition in definitions:
+        print(f"{definition['part_of_speech']}: {definition['definition_text']}")
+        print("")
