ca48fddb6412bf4c81217e95175d31e850dc026a
innowaluza <innowaluza@gmail.com>
2023-03-08 09:30:04 +0200

    it is what it is

---
+++ Dictionary.py
@@ -0,0 +1,17 @@
+import requests
+print("Dictionary")
+
+def get_word():
+    word = input("enter word")
+    return word
+
+def get_meaning(word):
+    data = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    response = data.json()
+    return response
+
+word = get_word()
+print(get_meaning(word))
+
+
+  
