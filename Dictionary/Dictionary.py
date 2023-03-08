9de882363eaafd7dae4f620e3790d0c67c10de50
innowaluza <innowaluza@gmail.com>
2023-03-08 11:00:30 +0200

    working

--- Dictionary/Dictionary.py
+++ Dictionary/Dictionary.py
@@ -9,16 +9,14 @@ def get_meaning(input):
     return response
 
 # Load the JSON data from a file or a string
-json_data = get_meaning(input) ##'[{"word": "hello", "phonetic": "həˈləʊ", "phonetics": [{"text": "həˈləʊ", "audio": "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"}, {"text": "hɛˈləʊ"}], "origin": "early 19th century: variant of earlier hollo ; related to holla.", "meanings": [{"partOfSpeech": "exclamation", "definitions": [{"definition": "used as a greeting or to begin a phone conversation.", "example": "hello there, Katie!", "synonyms": [], "antonyms": []}]}, {"partOfSpeech": "noun", "definitions": [{"definition": "an utterance of ‘hello’; a greeting.", "example": "she was getting polite nods and hellos from people", "synonyms": [], "antonyms": []}]}, {"partOfSpeech": "verb", "definitions": [{"definition": "say or shout ‘hello’.", "example": "I pressed the phone button and helloed", "synonyms": [], "antonyms": []}]}]}]'
+json_data = get_meaning(input) 
 
 # Load the JSON data as a Python object
 data = json.loads(json_data)
 
-# Extract the word and phonetic
+# Extract the word 
 word = data[0]["word"]
-#phonetic = data[0]["phonetic"]
 
-# Extract the definitions
 definitions = []
 for meaning in data[0]["meanings"]:
     part_of_speech = meaning["partOfSpeech"]
@@ -28,7 +26,6 @@ for meaning in data[0]["meanings"]:
 
 # Print the extracted data
 print(f"Word: {word}")
-##print(f"Phonetic: {phonetic}")
 print("Definitions:")
 for definition in definitions:
     print(f"{definition['part_of_speech']}: {definition['definition_text']}")
---
+++ Dictionary/READEME.md
@@ -0,0 +1,3 @@
+# Dictionary project
+
+A  dictionary python project that gets definitons of words from dictionary API
