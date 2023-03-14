82ec621e8ede53823677ac49e65a8bb486700bab
innowaluza <innowaluza@gmail.com>
2023-03-14 20:26:16 +0200

    added error box message

--- Dictionary/Graphic.py
+++ Dictionary/Graphic.py
@@ -1,9 +1,21 @@
 from tkinter import *
+from tkinter import messagebox
 root = Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
 
 def search():
-    return 2
+    try:
+    
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={api_key}"
 
+    except Exception as e:
+        #error messege
+        messagebox.showerror("Error, Word not found or no internet connection!")
+
+def display():
+    return 3
 
 message_label = Label(text='enter word',
                       font=('Arial', 16))
@@ -12,17 +24,17 @@ output_label = Label(font=('Arial', 16))
 
 entry = Entry(font=('Arial', 16), width=10)
 
-calc_button = Button(text='Ok',
+search_button = Button(text='Search',
                      font=('Arial', 16),
                      command = search())
 message_label.grid(row=0, column=0,)
 
-result_label =  Label(text = 'hello world',
+result_label =  Label(text = 'meaning will be here',
                       font = ('Arial', 16))
 
 entry.grid(row=0, column = 1)
 
-calc_button.grid(row=0, column = 2)
+search_button.grid(row=0, column = 2)
 
 ##output_label.grid(row=1, column=0, columnspan=3)
 
