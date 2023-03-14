27b1c85c0f211c832ef9b84e22c8331c13e8d860
innowaluza <innowaluza@gmail.com>
2023-03-14 12:45:48 +0200

    restructuring code

--- Dictionary/Graphic.py
+++ Dictionary/Graphic.py
@@ -9,18 +9,25 @@ message_label = Label(text='enter word',
                       font=('Arial', 16))
 
 output_label = Label(font=('Arial', 16))
+
 entry = Entry(font=('Arial', 16), width=10)
+
 calc_button = Button(text='Ok',
                      font=('Arial', 16),
                      command = search())
 message_label.grid(row=0, column=0,)
+
+result_label =  Label(text = 'hello world',
+                      font = ('Arial', 16))
+
 entry.grid(row=0, column = 1)
+
 calc_button.grid(row=0, column = 2)
+
 ##output_label.grid(row=1, column=0, columnspan=3)
 
 word = entry.get()
-result_label =  Label(text = 'hello world',
-                      font = ('Arial', 16))
+
 
 result_label.grid(row=1, column = 0)
 
